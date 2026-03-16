57c684a00110229752c8ca649c525b565202bf59
Lucas <lucasdush5@gmail.com>
2026-03-16 20:48:11 -0300

    ds

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require ('express')
 const cors = require ('cors')
-const { sequelize, DataTypes, Sequelize} = require ('sequelize')
+const { sequelize, DataTypes} = require ('sequelize')
 
 //configuração da conexao com o banco de dados
 
 const sequelize = new Sequelize('db_projeto', 'root', '',{
     host: 'localhost',
     dialect: 'mysql'
+})
+
+
+const Cliente = sequelize.define('CLiente', {
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    telefone: {
+        type: DataTypes.STRING,
+
+    }
 })
